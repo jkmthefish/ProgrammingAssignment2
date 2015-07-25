a762c3a2ac8e37d92841241cbaacf702cc74c878
jkmthefish <jkmthefish@users.noreply.github.com>
2015-07-24 22:10:27 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,16 @@
-## This function looks for the cache and creates if it does not already exist. 
+## This function looks for the cache and creates if it does not already exist. Also creates a list of needed functions for later.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function (y) {
+                x<<- Y       
+                s<<- NULL      ## restores Null
+        }
+        get <- function()x
+        solveMatrix <- function(mean) s <<- solve  ##function for inverting the matrix
+        getInvertMatrix <- function() s   ## creates spot for new Inverted matrix to go
+        list(set = set, get = get,  solveMatrix = solveMatrix, getInvertMatrix = getInvertMatrix)  ## Puts it all together
+                    
 }
 
 
